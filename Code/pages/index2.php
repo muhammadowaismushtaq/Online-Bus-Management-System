e4a77fc783f9ade8751f56152421a38ab2e2845e
Muhammad Owais Mushtaq <58966330+muhammadowaismushtaq@users.noreply.github.com>
2023-02-25 22:04:32 -0800

    Add files via upload

---
+++ Code/pages/index2.php
@@ -0,0 +1,19 @@
+<?php
+include 'connection.php';
+include 'topnav.php';
+?>
+
+          <!-- Breadcrumbs-->
+          <ol class="breadcrumb"
+            <li class="breadcrumb-item" style="background-image: linear-gradient">
+              <a href="index2.php"> <h2 style= "color: #8b0000" > WelCome to OBMS! </h2> </a>
+            </li>
+           
+          </ol>
+
+          <div class="bus_logo">
+            <a class="image full"><img src="../images/bus.jpg" style="width:950px;" style="background-attachment: fixed;" style="background-size: contain;"  > </a>
+
+    
+
+<?php include 'footer.php'; ?>
